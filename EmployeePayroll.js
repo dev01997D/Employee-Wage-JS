f15f428f155815a3ab14c6096ef47415a1b57301
dev01997D <devnandan01997@gmail.com>
2020-11-09 19:24:43 +0530

    Added: Employee wage is calculated for month of max working days as 20 functionality added

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,10 +1,11 @@
 {
-    // UC 3 : Using function and switch case
+    // UC 4 : Using function and switch case
     const IS_PART_TIME = 1;
     const IS_FULL_TIME = 2;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
+    const NUM_OF_WORKING_DAYS = 20;
 
     function getWorkingHours(empCheck) {
         let empHours = 0;
@@ -18,8 +19,10 @@
         }
     }
     let empHours = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHours = getWorkingHours(empCheck);
+    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        empHours += getWorkingHours(empCheck);
+    }
     let empWage = empHours * WAGE_PER_HOUR;
-    console.log("Hours : " + empHours + " Emp Wage : " + empWage);
+    console.log("Total Hours : " + empHours + " Emp Wage : " + empWage);
 }
