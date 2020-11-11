0f6c8272b0c2e0a369d692d29397c38eecaccdeb
dev01997D <devnandan01997@gmail.com>
2020-11-11 20:44:52 +0530

    Created: EmployeePayrollData class is created with attributes name, id, salary. Objects of class is also created

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,72 +1,36 @@
 {
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const WAGE_PER_HOUR = 20;
-    const NUM_OF_WORKING_DAYS = 20;
-    const MAX_HOURS_IN_MONTH = 100;
+    class EmployeePayrollData{
+        //property
+        id;
+        salary;
 
-    let totalEmpHours = 0;
-    let totalWorkingDays = 0;
-    let empdailyHoursAndWageArray = new Array();
-    while (totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-        let empHours = 0;
-        totalWorkingDays++;
-        let empCheck = Math.floor(Math.random() * 10) % 3;
-        empHours = getWorkingHours(empCheck);
-        totalEmpHours += empHours;
-        //Using js object to hold daily wage, day num and wage earned into an array
-        empdailyHoursAndWageArray.push({
-            dayNum: totalWorkingDays,
-            dailyHours: empHours,
-            dailyWage: calculateDailyWage(empHours),
-            toString() {
-                return '\nDay ' + this.dayNum + " => Working Hours is : " + this.dailyHours + " => And wage earned is : " + this.dailyWage;
-            }
-        });
-    }
-    function getWorkingHours(empCheck) {
-        //let empHours = 0;
-        switch (empCheck) {
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
-            case IS_FULL_TIME:
-                return FULL_TIME_HOURS;
-            default:
-                return 0;
+        //constructors
+        constructor(id, name, salary){
+            this.id=id;
+            this.name=name;
+            this.salary=salary;
+        }
+
+        //getters and Setters
+        get name(){
+            return this._name;
+        }
+        set name(name){ 
+            this._name=name;
+        }
+
+        //Method
+        toString(){
+            return "Id : "+this.id +" Name : "+this.name +" Salary : "+this.salary;
         }
     }
-    function calculateDailyWage(empHours) {
-        return empHours * WAGE_PER_HOUR;
-    }
 
-    //UC 10 - Ability to store the Day,Hours Worked and Wage Earned in a single object.
-   // console.log("Showing daily hours worked and wage earned : " + empdailyHoursAndWageArray);
+    //Creating object of class
+    let employeePayrollData=new EmployeePayrollData(1, "Mark", 2500.00);
+    console.log(employeePayrollData.toString());
 
-    //UC 11-A : Calculate total hours and total wage earned
-    let totalWages = empdailyHoursAndWageArray.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
-                    .reduce((totalWage, dailyHrsAndWage) => totalWage += dailyHrsAndWage.dailyWage, 0);
-    let totalHours = empdailyHoursAndWageArray.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours > 0)
-                     .reduce((totalHour, dailyHrsAndWage) => totalHour += dailyHrsAndWage.dailyHours, 0);
-    console.log("Total Wage : " + totalWages);
-    console.log(("Total hours Worked : " + totalHours));
-
-    //11-B :  Show full working days using forEach
-    console.log("Logging full woork Days");
-    empdailyHoursAndWageArray.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours ==8)
-                              .forEach(dailyHrsAndWage =>console.log(dailyHrsAndWage.toString()));
-
-    //11-C Show part time working using map by reducing to array
-    let partWorkingDayArr = empdailyHoursAndWageArray
-                            .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours==4)
-                            .map(dailyHrsAndWage => dailyHrsAndWage.toString());
-    console.log("Logging part time days :" +partWorkingDayArr);  
-    
-    //11-D Storing the day number where no working day using map function
-    let nonWorkingDays=empdailyHoursAndWageArray
-                       .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
-                       .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
-    console.log("Non working days are : "+nonWorkingDays);
-
+    //Change the value of name and id and print it
+    employeePayrollData.name="John";
+    employeePayrollData.id=2;
+    console.log(employeePayrollData.toString());
 }
