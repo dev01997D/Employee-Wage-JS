0b2786fe4e6e9a5a60aff7377fc20a03c7844377
dev01997D <71708110+dev01997D@users.noreply.github.com>
2021-05-27 16:11:54 +0530

    Update EmployeePayroll.js

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,5 +1,5 @@
-{
-    // UC 5 : Using function, while condition, and switch case
+{ // UC 2 : Using switch case
+    const IS_NO_TIME = 0;
     const IS_PART_TIME = 1;
     const IS_FULL_TIME = 2;
     const PART_TIME_HOURS = 4;
@@ -8,24 +8,27 @@
     const NUM_OF_WORKING_DAYS = 20;
     const MAX_HOURS_IN_MONTH = 100;
 
-    let totalEmpHours = 0;
-    let totalWorkingDays = 0;
-    while (totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-        totalWorkingDays++;
-        let empCheck = Math.floor(Math.random() * 10) % 3;
-        totalEmpHours += getWorkingHours(empCheck);
-    }
-    function getWorkingHours(empCheck) {
-        let empHours = 0;
-        switch (empCheck) {
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
-            case IS_FULL_TIME:
-                return FULL_TIME_HOURS;
-            default:
-                return 0;
+    {
+        function getDailyWorkingHours(empCheck) {
+            switch (empCheck) {
+                case IS_PART_TIME: return PART_TIME_HOURS;
+                case IS_FULL_TIME: return FULL_TIME_HOURS;
+                default:
+                    return 0;
+            }
         }
+
+        let totalEmpHours = 0;
+        let totalWorkingDays = 0;
+
+        while (totalEmpHours < MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+            totalEmpHours += getDailyWorkingHours(empCheck);
+        }
+
+
+        let totalEmpWage = totalEmpHours * WAGE_PER_HOUR;
+        console.log("Total Days : " + totalWorkingDays + "\t\tTotal Hours : " + totalEmpHours + "\tTotal Emp Wage : " + totalEmpWage);
     }
-    let empWage = totalEmpHours * WAGE_PER_HOUR;
-    console.log("Total Days : " + totalWorkingDays + "\tTotal Hours : " + totalEmpHours + "\tEmp Wage : " + empWage);
 }
