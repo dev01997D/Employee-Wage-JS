fa1630a61b0c590dda237fc262d11ef9ab5117f6
dev01997D <71708110+dev01997D@users.noreply.github.com>
2021-05-27 16:02:34 +0530

    Update EmployeePayroll.js

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,5 +1,5 @@
-{
-    // UC 4 : Using function and switch case
+{ // UC 2 : Using switch case
+    const IS_NO_TIME = 0;
     const IS_PART_TIME = 1;
     const IS_FULL_TIME = 2;
     const PART_TIME_HOURS = 4;
@@ -7,22 +7,24 @@
     const WAGE_PER_HOUR = 20;
     const NUM_OF_WORKING_DAYS = 20;
 
-    function getWorkingHours(empCheck) {
-        let empHours = 0;
-        switch (empCheck) {
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
-            case IS_FULL_TIME:
-                return FULL_TIME_HOURS;
-            default:
-                return 0;
+    {
+        function getDailyWorkingHours(empCheck) {
+            switch (empCheck) {
+                case IS_PART_TIME: return PART_TIME_HOURS;
+                case IS_FULL_TIME: return FULL_TIME_HOURS;
+                default:
+                    return 0;
+            }
         }
+
+        let totalEmpHours = 0;
+        for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+            totalEmpHours += getDailyWorkingHours(empCheck);
+        }
+
+        let totalEmpWage = totalEmpHours * WAGE_PER_HOUR;
+
+        console.log("Hours : " + totalEmpHours + " Emp Wage : " + totalEmpWage);
     }
-    let empHours = 0;
-    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-        let empCheck = Math.floor(Math.random() * 10) % 3;
-        empHours += getWorkingHours(empCheck);
-    }
-    let empWage = empHours * WAGE_PER_HOUR;
-    console.log("Total Hours : " + empHours + " Emp Wage : " + empWage);
 }
